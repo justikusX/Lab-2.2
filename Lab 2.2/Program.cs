acffa222a188184e70fdef0df442eb3be6492bd9
Илья <@DESKTOP-VPI29MR>
2024-10-08 17:16:37 +0200

    Добавьте файлы проекта.

---
+++ Lab 2.2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Ввод координат трех вершин
+Console.WriteLine("Введите координаты трех вершин прямоугольника:");
+Console.Write("x1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("x2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.Write("x3: ");
+int x3 = int.Parse(Console.ReadLine());
+Console.Write("y3: ");
+int y3 = int.Parse(Console.ReadLine());
+
+// Определяем координаты четвертой вершины
+int x4 = GetFourthCoordinate(x1, x2, x3);
+int y4 = GetFourthCoordinate(y1, y2, y3);
+
+Console.WriteLine($"Координаты четвертой вершины: ({x4}, {y4})");
+
+int GetFourthCoordinate(int a, int b, int c)
+{
+if (a == b)
+    return c;
+else if (a == c)
+    return b;
+else
+    return a;
+}
